37e7cbc9cf21e48503cd26e1e48d32702def34fe
david allgr <david.allgr@eduge.ch>
2025-12-12 10:37:43 +0100

    Journal fait

--- magasin/JOURNAL.md
+++ magasin/JOURNAL.md
@@ -1,1 +1,7 @@
-12.12.2025
+# Friday 12.12.2025
+* Creation de la base de donnee.
+`Toutes les requtes sql mais pas encore avec le INNER JOIN.` 
+* Strucure de base du site. 
+`Mais je pense modifier car je trouve pas assez facile a utiliser.` 
+* info_api commencer. 
+* .htaccess configurer.
---
+++ magasin/info_api.php
@@ -0,0 +1,34 @@
+<h1>Info api</h1>
+
+<h2>User</h2>
+
+<h3>Create user</h3>
+<p>You can create a user</p>
+
+<h3>Edit user</h3>
+<p>You can edit info's user</p>
+
+<h3>Delete user</h3>
+<p>You can delete user</p>
+
+<h2>Article</h2>
+
+<h3>Create article</h3>
+<p>You can create a article</p>
+
+<h3>Edit article</h3>
+<p>You can edit info's article</p>
+
+<h3>Delete article</h3>
+<p>You can delete article</p>
+
+<h2>Categorie</h2>
+
+<h3>Create Categorie</h3>
+<p>You can create a Categorie</p>
+
+<h3>Edit Categorie</h3>
+<p>You can edit info's Categorie</p>
+
+<h3>Delete Categorie</h3>
+<p>You can delete Categorie</p>
